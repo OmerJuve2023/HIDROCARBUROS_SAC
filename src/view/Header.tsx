c0044fc4fb6a85861c1ef46d104e2d0d7507bfd8
omerJuve23 <0333182023@unjfsc.edu.pe>
2023-10-07 22:28:10 -0500

    update Header.tsx and ListaDeProductos.tsx; add ListaPedidos.tsx

--- src/view/Header.tsx
+++ src/view/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
                         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                             <li><a href="#" className="nav-link px-4 link-secondary">HIDROCARBUROS</a></li>
                             <li><a href="#" className="nav-link px-2 link-body-emphasis">Productos</a></li>
-                            <li><a href="#" className="nav-link px-2 link-body-emphasis">Inventario</a></li>
+                            <li><a href="#" className="nav-link px-2 link-body-emphasis">Pedido</a></li>
                         </ul>
 
                         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
--- src/view/ListaDeProductos.tsx
+++ src/view/ListaDeProductos.tsx
@@ -1,7 +1,28 @@
 import "../styles/list_products_styles.css"
 import {ListProducts} from "../service/ListProducts.ts";
+import {useState} from "react";
 
 export function ListaDeProductos() {
+
+    const [selectedProducts, setSelectedProducts]
+        = useState<Product[]>([]);
+
+    const agregarProducto = (producto: Product) => {
+        // Verificar si el producto ya está en la lista
+        const productoExistente
+            = selectedProducts.find((p) => p.name === producto.name);
+
+        if (!productoExistente) {
+            // Solo agregar si no existe en la lista
+            const nuevaLista = [...selectedProducts, producto];
+            setSelectedProducts(nuevaLista);
+            // Almacenar la lista actualizada en el localStorage
+            localStorage.setItem('miLista', JSON.stringify(nuevaLista));
+        }else{
+            alert("ya has agregado este item a la lista")
+        }
+    };
+
     return (
         <>
             {ListProducts.map((producto, index) => (
@@ -17,7 +38,8 @@ export function ListaDeProductos() {
                                 <h6 className="custom-card-title">{producto.name}</h6>
                             </div>
                             <div className="d-grid gap-2">
-                                <button className="btn btn-primary">
+                                <button className="btn btn-primary"
+                                        onClick={() => agregarProducto(producto)}>
                                     Agregar
                                 </button>
                             </div>
